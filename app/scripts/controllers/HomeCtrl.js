9a82990feee657b4bd780a1dbcede2ae1c58290f
Kevin Wood <krwood90@gmail.com>
2017-06-04 09:34:08 -0500

    bloc chat list messages completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,18 @@
 (function() {
-    function HomeCtrl(Room, ModalCtrl) {
+    function HomeCtrl(Room, Message, $scope, ModalCtrl) {
+        $scope.currentRoom = null;
+        $scope.messages = null;
+
         var home = this;
         home.rooms = Room.all;
+
+        home.selectRoom = function(room) {
+            $scope.currentRoom = room;
+            $scope.messages = Message.getByRoomId(room.$id);
+        };
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$scope', HomeCtrl]);
 })();
